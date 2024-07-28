6db720189c3f7d027eb3cae1facd66a88d994e6e
umaru <15875339926@139.com>
2024-07-28 12:46:39 +0800

    GIT COMMIT MESSAGE:
    chore: 更新 embedding 处理和相关依赖项
    
    - 将 `langchain`、`langchain-community` 和 `fastembed` 依赖项更新到新版本
    - 在 `fast_embed_runnable.py` 中导入 `numpy` 和 `TextEmbedding`
    - 更改 embedding 实例化方式以使用 `TextEmbedding`
    - 修改 `execute` 方法以处理嵌套结果列表并将其转换为扁平列表
    
    Signed-off-by: umaru <15875339926@139.com>

--- runnable/fast_embed_runnable.py
+++ runnable/fast_embed_runnable.py
@@ -1,17 +1,21 @@
 from typing import List
 
+import numpy as np
 from langchain_community.embeddings import FastEmbedEmbeddings
 from langchain_core.runnables import RunnableLambda
+from fastembed import TextEmbedding
 
 from core.server_settings import server_settings
 
 
 class FastEmbedRunnable:
     def __init__(self):
-        self.embedding = FastEmbedEmbeddings(model_name=server_settings.model_name, cache_dir="models")
+        self.embedding = TextEmbedding(model_name=server_settings.model_name, cache_dir="models")
 
     def instance(self):
         return RunnableLambda(self.execute).with_types(input_type=str, output_type=List[float])
 
     def execute(self, text: str) -> List[float]:
-        return self.embedding.embed_query(text)
+        result = self.embedding.embed(text)
+        result = [item for sublist in result for item in sublist.tolist()]
+        return result
